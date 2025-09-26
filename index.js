7f9d250d0e75c4b09538bc7180d11c28d7032013
alexiswright02 <misslexiW@gmail.com>
2025-09-26 12:11:52 -0400

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,60 @@
-// === Constants ===
+// === ENDPOINTS ===
 const BASE = "https://fsa-puppy-bowl.herokuapp.com/api";
-const COHORT = "/"; // Make sure to change this!
+const COHORT = "/2503-example"; //replace with name if i can get it to work
 const API = BASE + COHORT;
+
+// === STATE ===
+let puppies = [];
+let selectedPuppy = {};
+
+// === ASYNC FUNCTIONS ===
+
+//1. getPuppies
+async function getPuppies() {
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//2. getSelectedPuppy
+async function getSelectedPuppy(id) {
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//3. removePuppy
+async function removePuppy(id) {
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//4. addNewPuppy
+async function addNewPuppy(puppyData) {
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//=== COMPONENT FUNCTIONS ===
+
+//1. Full Roster
+
+//2. Selected Puppy Details
+
+//3. New Puppy Submission Form
+
+//=== RENDERING FUNCTIONS ===
+
+//1. Renders the component functions to the index page
+
+//=== INITIALIZATION ===
+
+//1. invoke getPuppies function
+
+//2. invoke init function
